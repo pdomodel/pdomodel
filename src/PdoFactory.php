531c4d1a5ec8c6ea083f7cc67e901cd8b2d373dc
Andrey Reinwald <andreinwald@gmail.com>
2018-06-07 07:22:39 +0300

    Added default driver, charset and options to PdoFactory

--- src/PdoFactory.php
+++ src/PdoFactory.php
@@ -8,15 +8,29 @@ class PdoFactory
 {
     public static function createConnection(array $config)
     {
+        $driver = isset($config['driver']) ? $config['driver'] : 'mysql';
+        $charset = isset($config['charset']) ? $config['charset'] : 'utf8';
+
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+            PDO::ATTR_CASE => PDO::CASE_LOWER,
+        ];
+        if (isset($config['options']) && is_array($config['options'])) {
+            $options =  $config['options'] + $options;
+        }
+
         return new PDO(implode(';',
             [
-                $config['driver'] . ':host=' . $config['host'],
+                $driver . ':host=' . $config['host'],
                 'dbname=' . $config['database'],
-                'charset=' . $config['charset'],
+                'charset=' . $charset,
             ]),
             $config['username'],
             $config['password'],
-            isset($config['options']) ? $config['options'] : null
+            $options
         );
     }
 }
