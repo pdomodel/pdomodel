240c81b866272747cb88f99c807a372b1b379c75
Andrey Reinwald <andreinwald@gmail.com>
2021-02-21 19:26:35 +0200

    New PDOFactory with fixed parameters and port

--- src/PdoFactory.php
+++ src/PdoFactory.php
@@ -6,31 +6,34 @@ use \PDO;
 
 class PdoFactory
 {
-    public static function createConnection(array $config)
+    public static function createConnection(
+        string $host = '127.0.0.1',
+        string $database = 'defaultdb',
+        string $user = 'root',
+        string $password = '',
+        array $options = [],
+        int $port = 3306,
+        string $charset = 'utf8',
+        string $driver = 'mysql'
+    )
     {
-        $driver = isset($config['driver']) ? $config['driver'] : 'mysql';
-        $charset = isset($config['charset']) ? $config['charset'] : 'utf8';
-
-        $options = [
+        $defaultOptions = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
             PDO::ATTR_CASE => PDO::CASE_LOWER,
         ];
-        if (isset($config['options']) && is_array($config['options'])) {
-            $options =  $config['options'] + $options;
-        }
 
-        return new PDO(implode(';',
-            [
-                $driver . ':host=' . $config['host'],
-                'dbname=' . $config['database'],
-                'charset=' . $charset,
-            ]),
-            $config['username'],
-            $config['password'],
-            $options
-        );
+        $options = array_merge($defaultOptions, $options);
+
+        $connectionString = implode(';', [
+            $driver . ':host=' . $host,
+            'dbname=' . $database,
+            'charset=' . $charset,
+            'port=' . $port,
+        ]);
+
+        return new PDO($connectionString, $user, $password, $options);
     }
 }
