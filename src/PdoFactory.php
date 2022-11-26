9851b6b344ce451a159c1a3eca97f95f162b673c
Andrey Reinwald <andreinwald@gmail.com>
2022-11-26 20:04:09 +0200

    Change default charset to utf8mb4

--- src/PdoFactory.php
+++ src/PdoFactory.php
@@ -13,7 +13,7 @@ class PdoFactory
         string $password = '',
         array $options = [],
         int $port = 3306,
-        string $charset = 'utf8',
+        string $charset = 'utf8mb4',
         string $driver = 'mysql'
     )
     {
@@ -21,7 +21,7 @@ class PdoFactory
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
             PDO::ATTR_CASE => PDO::CASE_LOWER,
         ];
 
