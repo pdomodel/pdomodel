0ab75f8eea48e74dc4ac5e3946b6ac8746beeb39
andreinwald <andreinwald@gmail.com>
2023-05-28 14:16:40 +0200

    Moved tests to SQLite inMemory db

--- tests/InsertTest.php
+++ tests/InsertTest.php
@@ -7,13 +7,17 @@ class InsertTest extends TestCase
 {
     public function testInsert(): void
     {
-        $db = new Pseudo\Pdo();
-        $model = (new PdoModel($db))->setTable('test_table');
-
-        $targetData = [['id' => 1, 'foo' => 'bar']];
-        $db->mock("SELECT id FROM test_table WHERE foo=?", $targetData, ['bar']);
-        $resultData = $model->select(['foo' => 'bar'], columns: ['id'])->all();
-
+        $targetData = ['id' => 1, 'foo' => 'bar'];
+        $testDb = new \PDO('sqlite::memory:');
+        $testDb->exec('
+            CREATE TABLE test_table (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                foo VARCHAR(255) NOT NULL
+            )
+        ');
+        $modelSqlite = (new PdoModel($testDb))->setTable('test_table');
+        $modelSqlite->insert($targetData);
+        $resultData = $modelSqlite->select(['foo' => 'bar'])->first();
         $this->assertEquals($targetData, $resultData);
     }
 }
