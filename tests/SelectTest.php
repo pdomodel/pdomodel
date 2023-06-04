ecb9057d6c93ceb0a572302bd5824bcac85631ae
andreinwald <andreinwald@gmail.com>
2023-06-04 13:32:27 +0200

    Created test for select

---
+++ tests/SelectTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use PdoModel\PdoModel;
+use PHPUnit\Framework\TestCase;
+
+class SelectTest extends TestCase
+{
+    public function testSelect(): void
+    {
+        $insertData = [
+            ['foo' => 'cut by where', 'height' => 11, 'day' => 2],
+            ['foo' => 'ok', 'height' => 10, 'day' => 2],
+            ['foo' => 'ok', 'height' => 10, 'day' => 2],
+        ];
+        $targetResult = [['foo' => 'ok', 'height' => 20]];
+
+
+        $model = new PdoModel(new \PDO('sqlite::memory:'));
+        $model->createTable('test_table')
+            ->column('id', autoIncrement: true, primaryKey: true)
+            ->column('height')
+            ->column('foo', type: 'string')
+            ->column('day')
+            ->execute();
+        $model->setTable('test_table');
+
+        foreach ($insertData as $row) {
+            $model->insert($row);
+        }
+
+        $result = $model->select(
+            ['height' => 10],
+            'id desc',
+            3,
+            0,
+            'day',
+            ['foo', 'sum(height) as height']
+        )->all();
+        $this->assertEquals($targetResult, $result);
+    }
+}
